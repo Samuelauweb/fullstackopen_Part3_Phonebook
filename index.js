c1da2471cf59d4a5852618cfa4c9fb55326d2c19
Samuel Lau <samuel.cflau@gmail.com>
2021-11-16 18:05:21 +0800

    Ex3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let phonebook = [
   {
@@ -23,24 +26,27 @@ let phonebook = [
     number: '39-23-6423122',
   },
 ]
-
+// Home page
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// GET all phones
 app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 })
 
+// GET a specific page
 app.get('/info', (request, response) => {
   response.send(`
   <p>Phonebook has info for ${phonebook.length} people</p>
   <p>${new Date()}</p>`)
 })
 
+// GET a phone
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const phone = phonebook.find(p => {
+  const phone = phonebook.find((p) => {
     // console.log(p.id, typeof p.id, id, typeof id, p.id === id);
     return p.id === id
   })
@@ -51,10 +57,45 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).send('The phone you are looking for does not exist!')
   }
-  
+})
+
+// POST or create a phone
+const generateId = () => {
+  const maxId =
+    phonebook.length > 0 ? Math.floor(Math.random(...phonebook.map((p) => p.id))*1000) : 0
+  return maxId + 1
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log("body:", body)
+
+  if(!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const phone = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(phone)
+  console.log('phonebook:', phonebook)
+
+  response.json(phone)
+})
+
+// DELETE a phone
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter((p) => p.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT,() => {
-  console.log(`Server running on PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`)
 })
