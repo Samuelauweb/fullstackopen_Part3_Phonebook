17101dca58e48748f4ccc0fbcc6b34eb1c8f2025
Samuel <samuel.cflau@gmail.com>
2021-12-01 18:14:44 +0800

    Ex3.13: GET - Getting an individual note (findById)

--- index.js
+++ index.js
@@ -30,29 +30,6 @@ morgan.format(
 
 app.use(morgan('postFormat'))
 
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
 // GET all phones
 app.get('/api/persons', (request, response) => {
   Phone.find({}).then(phone => {
@@ -69,18 +46,19 @@ app.get('/info', (request, response) => {
 
 // GET a phone
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const phone = phonebook.find((p) => {
-    // console.log(p.id, typeof p.id, id, typeof id, p.id === id);
-    return p.id === id
-  })
+  // const id = Number(request.params.id)
+  // const phone = phonebook.find((p) => {
+  //   // console.log(p.id, typeof p.id, id, typeof id, p.id === id);
+  //   return p.id === id
+  // })
+  Phone.findById(request.params.id).then(phone => response.json(phone))
 
-  if (phone) {
-    // console.log("phone:",phone);
-    response.json(phone)
-  } else {
-    response.status(404).send('The phone you are looking for does not exist!')
-  }
+  // if (phone) {
+  //   // console.log("phone:",phone);
+  //   return phone
+  // } else {
+  //   response.status(404).send('The phone you are looking for does not exist!')
+  // }
 })
 
 // POST or create a phone
